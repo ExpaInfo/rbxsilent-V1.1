f2b6088f5cfcc6c5d46637eb32264d69d2725384
Expa Info <cebsxifnrjdhso639@gmail.com>
2026-03-12 00:44:31 +1100

    Update Main.cpp to hide Roblox windows
    Replaced the original comment block with a new one and added Win32 API functionality to hide Roblox windows.

--- rbxsilent/Main.cpp
+++ rbxsilent/Main.cpp
@@ -7,4 +7,41 @@ Author tostring#1337 and Expainfo
 ----------------------------------------------
 */
 
-sorry that was me fucking with possable changes
+#include <Windows.h>
+
+// Write a string to the console using only Win32 API (no CRT I/O overhead)
+static void ConWrite(const char* msg) {
+    DWORD written;
+    WriteConsoleA(GetStdHandle(STD_OUTPUT_HANDLE), msg, lstrlenA(msg), &written, NULL);
+}
+
+static BOOL CALLBACK windcallback(HWND hWnd, LPARAM lparam) {
+    if (!IsWindowVisible(hWnd))
+        return TRUE;
+
+    char buffer[16];
+    int len = GetWindowTextA(hWnd, buffer, sizeof(buffer));
+
+    // Compare without any CRT - just manual byte check
+    if (len == 6 &&
+        buffer[0] == 'R' && buffer[1] == 'o' && buffer[2] == 'b' &&
+        buffer[3] == 'l' && buffer[4] == 'o' && buffer[5] == 'x') {
+        ShowWindow(hWnd, SW_HIDE);
+        PostMessage(hWnd, WM_SYSCOMMAND, SC_MINIMIZE, 0);
+        ConWrite("Found roblox window\n");
+    }
+
+    return TRUE;
+}
+
+int main() {
+    ConWrite("All roblox clients that are open/opened will now be hidden.\n");
+
+    SetPriorityClass(GetCurrentProcess(), IDLE_PRIORITY_CLASS);
+
+    while (true) {
+        EnumWindows(windcallback, 0);
+        Sleep(2000);
+    }
+    return 0;
+}
