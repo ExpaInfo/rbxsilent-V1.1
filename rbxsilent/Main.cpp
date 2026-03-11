828ef003455ba15805d9a2307441f1ab4d2157f6
Expa Info <cebsxifnrjdhso639@gmail.com>
2026-03-11 22:43:19 +1100

    Remove window enumeration logic from Main.cpp
    Removed the original window enumeration logic and added a comment indicating changes.

--- rbxsilent/Main.cpp
+++ rbxsilent/Main.cpp
@@ -7,26 +7,4 @@ Author tostring#1337 and Expainfo
 ----------------------------------------------
 */
 
-#include <Windows.h>
-#include <cstring>
-
-static BOOL CALLBACK WindowEnumProc(HWND hWnd, LPARAM) {
-    if (!IsWindowVisible(hWnd)) return TRUE;
-
-    char title[256] = {};
-    int len = GetWindowTextA(hWnd, title, sizeof(title));
-    if (len <= 0) return TRUE;
-
-    if (strcmp(title, "SomeWindowTitle") == 0) {
-        // Do one lightweight action here
-    }
-
-    return TRUE;
-}
-
-int main() {
-    while (true) {
-        EnumWindows(WindowEnumProc, 0);
-        Sleep(1000);
-    }
-}
+sorry that was me fucking with possable changes
